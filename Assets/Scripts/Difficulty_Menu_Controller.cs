0400d46b95e7d26e2f19f2e7286261f34fba6ace
eiparfenov <epar2002@outlook.com>
2021-02-06 13:36:50 +0300

    Add main and difficulty menu

---
+++ Assets/Scripts/Difficulty_Menu_Controller.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Difficulty_Menu_Controller : MonoBehaviour
+{
+    public static string selected_difficulty = "Practice";
+
+    [SerializeField] GameObject main_Menu;
+    [SerializeField] GameObject difficulty_Menu;
+
+    public void Back_Button_Press()
+    {
+        main_Menu.SetActive(true);
+        difficulty_Menu.SetActive(false);
+    }
+
+    public void Practice_Button_Press()
+    {
+        selected_difficulty = "Practice";
+        Open_Mode_Menu();
+    }
+    public void Easy_Button_Press()
+    {
+        selected_difficulty = "Easy";
+        Open_Mode_Menu();
+    }
+    public void Midle_Button_Press()
+    {
+        selected_difficulty = "Midle";
+        Open_Mode_Menu();
+    }
+    public void Hard_Button_Press()
+    {
+        selected_difficulty = "Hard";
+        Open_Mode_Menu();
+    }
+    private void Open_Mode_Menu()
+    {
+        Debug.Log(Difficulty_Menu_Controller.selected_difficulty);
+    }
+}
---
+++ Assets/Scripts/Main_Menu_Controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_Menu_Controller : MonoBehaviour
+{
+    [SerializeField] GameObject main_Menu;
+    [SerializeField] GameObject difficulty_Menu;
+
+    public void Play_Button_Press()
+    {
+        main_Menu.SetActive(false);
+        difficulty_Menu.SetActive(true);
+    }
+
+    public void Quit_Button_Press()
+    {
+        Application.Quit();
+    }
+}
