a4417b84b007a1eacff5c03f3592b008af81f01f
eiparfenov <epar2002@outlook.com>
2021-02-07 11:36:50 +0300

    Add Dart

---
+++ Assets/Scripts/Dart_In_Aim_Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dart_In_Aim_Controller : MonoBehaviour
+{
+    [SerializeField] GameObject dart;
+
+    public void Activate_Dart(Vector2 finger_Position)
+    {
+        dart.SetActive(true);
+        Vector3 position = new Vector3(finger_Position.x, finger_Position.y, 0);
+        dart.transform.localPosition = position;
+    }
+
+    public void Move_Dart(Vector2 finger_Position)
+    {
+        Vector3 position = new Vector3(finger_Position.x, finger_Position.y, 0);
+        dart.transform.localPosition = position;
+    }
+
+    public void Deactivate_Dart()
+    {
+        dart.SetActive(false);
+    }
+}
--- Assets/Scripts/Game_Controller.cs
+++ Assets/Scripts/Game_Controller.cs
@@ -7,23 +7,30 @@ public class Game_Controller : MonoBehaviour
     [SerializeField] GameObject pointer;
     Pointer_Controller pointer_Controller;
 
+    [SerializeField] GameObject dart_Zero_Position;
+    Dart_In_Aim_Controller dart_In_Aim_Controller;
+
     private void Start()
     {
         pointer_Controller = pointer.GetComponent<Pointer_Controller>();
+        dart_In_Aim_Controller = dart_Zero_Position.GetComponent<Dart_In_Aim_Controller>();
     }
 
     public void Aim_Start(Vector2 finger_Position)
     {
         pointer_Controller.Activate_Pointer(finger_Position);
+        dart_In_Aim_Controller.Activate_Dart(finger_Position / 40);
     }
 
     public void Aim_Move(Vector2 finger_Position)
     {
         pointer_Controller.Move_Pointer(finger_Position);
+        dart_In_Aim_Controller.Move_Dart(finger_Position / 40);
     }
 
     public void Aim_Finish()
     {
         pointer_Controller.Deactivate_Pointer();
+        dart_In_Aim_Controller.Deactivate_Dart();
     }
 }
